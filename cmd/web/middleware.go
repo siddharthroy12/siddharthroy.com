a00f587d03d8d3fb106bcab45964411cf21f734d
Siddharth Roy <siddharthroy36912@gmail.com>
2025-08-29 11:10:20 +0530

    Image loads faster

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -45,7 +45,7 @@ func (app *application) saveAndLoadSession(next http.Handler) http.Handler {
 }
 func (app *application) authenticate(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" && strings.Contains(r.URL.Path, "/static") {
+		if r.Method == "GET" && (strings.Contains(r.URL.Path, "/static") || strings.Contains(r.URL.Path, "/media")) {
 			next.ServeHTTP(w, r)
 			return
 		}
